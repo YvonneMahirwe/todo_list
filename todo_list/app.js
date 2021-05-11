36a98152a8d8dab5e43b488478a1d5da677cd20f
YvonneMahirwe <73539015+YvonneMahirwe@users.noreply.github.com>
2021-05-11 12:34:30 -0400

    Update app.js

--- todo_list/app.js
+++ todo_list/app.js
@@ -2,8 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const _ = require("lodash");    
+const mongoose = require("mongoose");   
 
 const app = express();  
 
